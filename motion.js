171f1adae780698cd6fc67b963dc62fabd20ef7d
Payoshika <59700005+Payoshika@users.noreply.github.com>
2020-06-07 16:13:14 +0900

    minor js changes, adde basic css style

--- motion.js
+++ motion.js
@@ -1,3 +1,23 @@
+// global arrays
+var qTypeArray = ["add"]
+var question;
+var answer;
+var answerResult = []
+var highScore;
+var startTime = 10;
+var counter = startTime
+
+// displaying the current num-range value to user
+var numRangeValue =  document.getElementById("math-range").valueAsNumber
+$(".range-show").html(numRangeValue)
+
+// changing the num-range when user changes the slider value
+  $(document).on("input", "#math-range", function(){
+    var newValueText = $(this).val()
+    var newValueNum = Number(newValueText)
+    numRangeValue = newValueNum
+    $(".range-show").html(newValueNum)
+  })
 
 // add toggle class="active" to buttons if clicked
 $(document).on("click", ".choice", function(event){
@@ -16,19 +36,15 @@ $(document).on("click", ".choice", function(event){
   console.log(qTypeArray);
 });
 
+// game start button
 $(document).on("click", ".start-btn", function(){
   playGame()
 })
 
-var qTypeArray = ["add"]
-var question;
-var answer;
-var answerResult = []
-
 // 4 types of calculations random
 var qGenerator = function(){
-  var firstNum = Math.floor(Math.random()*10)
-  var secondNum = Math.floor(Math.random()*10)
+  var firstNum = Math.floor(Math.random()*(numRangeValue)+1)
+  var secondNum = Math.floor(Math.random()*(numRangeValue)+1)
   var randomToThree = Math.floor(Math.random()*qTypeArray.length)
   var calculation = qTypeArray[randomToThree]
   if(calculation === "add"){
@@ -39,16 +55,15 @@ var qGenerator = function(){
     question = `${firstNum} - ${secondNum}`
     answer = firstNum - secondNum
   }
-  else if(calculation === "devide") {
+  else if(calculation === "divide") {
     question = `${firstNum} / ${secondNum}`
-    answer = firstNum / secondNum
+    answer = Math.round(firstNum / secondNum)
   }
   else {
     question = `${firstNum} * ${secondNum}`
     answer = firstNum * secondNum
   }
 }
-
 
 // basic gameplay function
 var showQuestion = function(){
@@ -59,64 +74,83 @@ var showQuestion = function(){
   }
 }
 
-$(document).on("change", ".answer-box input", function(){
-  if(counter > 0){
-    var userAnswer = $(this).val();
-    if(Number(userAnswer) === answer){
-      $(".result-box span").text("Perfect!")
-      counter++;
-      answerResult.push(1)
-      $(".timer h2").html(counter)
+// user answer checking function
+$(document).on("keydown", ".answer-box input", function(event){
+  if(event.keyCode === 13){
+    if(counter > 0){
+      var userAnswer = $(this).val();
+      if(Number(userAnswer) === answer){
+        $(".result-box span").text("Perfect!")
+        counter++;
+        $(".timer h2").html(counter);
+          if(question.includes("-")){
+            answerResult.push(1.2 * numRangeValue)
+          }
+          else if(question.includes("*")){
+            answerResult.push(1.4 * numRangeValue)
+          }
+          else if(question.includes("/")){
+            answerResult.push(1.6 * numRangeValue)
+          }
+          else {
+            answerResult.push(1 * 10)
+          }
+          var currentScore = answerResult.reduce(function(total, val){
+              return total + val
+            },0)
+            $(".show-number:eq(1)").text(`${currentScore}`)
+        }
+      else {
+        $(".result-box span").text("you got it wrong!")
+        answerResult.push(0)
       }
-    else {
-      $(".result-box span").text("you got it wrong!")
-      answerResult.push(0)
+      question = "";
+      answer = "";
+      $(this).val("");
+      showQuestion();
     }
-    question = "";
-    answer = "";
-    $(this).val("");
-    showQuestion();
-
   }
 })
 
 
-var startTime = 10;
-var counter = startTime
 // basic countdown function
 var countDown = function(){
-  var initialTime = new Date();
-  var endTime = new Date(initialTime.getTime() +10*1000 )
   var timer = setInterval(function(){
     counter--;
-    $(".timer h2").html(counter)
-    initialTime = new Date();
-    // if(endTime.getTime() <= initialTime.getTime()){
-    //   clearInterval(timer);
-    //   $(".timer h2").html("Game Over")
-    // }
+    $(".timer").html(`<h2 class="mb-0" >${counter}</h2>`)
     if(counter <= 0){
       clearInterval(timer);
-      $(".timer h2").html("Game Over")
+      $(".timer").html(`<p class="mb-0" >Game Over</p>`)
       resultChecker();
       $(".start-btn p").text("play again?")
     }
   },1000)
 }
-
+// checking the result of the game and displaying.
 var resultChecker = function(){
   var totalAnswers = answerResult.length
   var correctAnswers = answerResult.filter(val => val > 0).length
-  var correctRatio = correctAnswers/totalAnswers*100
+  var correctRatio = Math.round(correctAnswers/totalAnswers*100) || 0
   var totalPoints = answerResult.reduce(function(total, val){
     return total + val
   },0)
-  console.log(`you got ${correctRatio}% right, you got ${totalPoints}`);
+  console.log(totalAnswers,correctAnswers,correctRatio,totalPoints);
+  if(highScore < totalPoints || highScore === undefined){
+    highScore = totalPoints
+  }
+    $(".show-number").first().text(highScore)
+    $(".show-number:eq(2)").text(`${totalAnswers}`)
+    $(".show-number").last().text(`${correctRatio}`)
   answerResult =[]
 }
 
+// basic flow of the game, 1.reset the counter, update the question, start the counter
 var playGame = function(){
-  countDown();
-  showQuestion();
   counter = 10;
+  showQuestion();
+  countDown();
 }
+
+$(document).on("load", function(){
+  showQuestion();
+})
